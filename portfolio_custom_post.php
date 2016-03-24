7fc1433a594583898b871d38ee95dcaf8b578f83
bismelheenakhan <hbk_55@hotmail.com>
2016-03-24 02:29:10 -0400

    start of widget

--- portfolio_custom_post.php
+++ portfolio_custom_post.php
@@ -35,3 +35,31 @@ function portfolio_posts() {
 add_action( 'init', 'portfolio_posts' ); // this registers the custom post type 
 
 
+// Create Widget
+class portfolio_post_widget extends WP_Widget {
+    function portfolio_post_widget() {
+        // widget settings
+        $widget_ops = array(
+			'classname' => 'Featured Images',
+			'description' => 'Display your most recent images.' );
+        // widget control settings
+        $control_ops = array( 'width' => 250, 'height' => 350, 'id_base' => 'portfolio_post_widget' );
+        // create the widget
+        $this->WP_Widget( 'portfolio_post_widget', 'Featured Images', $widget_ops, $control_ops );
+    }   
+    function widget( $args, $instance ) {
+         
+        extract( $args );
+ 
+        // User Settings
+        $display = $instance['display'];
+        $showportfolio = $instance['showportfolio'];
+        $numberPosts = $instance['numberPosts'];        
+         
+        if($display=="older-posts") {
+            $feedsort="meta_value";
+            $metakey="&meta_key=Older";
+        } else {
+            $feedsort="date";
+            $metakey="";
+        }
