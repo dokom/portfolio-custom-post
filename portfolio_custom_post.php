217882b86224029a7a0aa88710bc57712f47665b
hy <ironhci@gmail.com>
2016-03-24 00:48:20 -0400

    made comments

--- portfolio_custom_post.php
+++ portfolio_custom_post.php
@@ -10,28 +10,28 @@
 // Create Custom Post Type
 function portfolio_posts() {
 	$args = array(
-		'label' => 'Portfolio',
-		'public' => true,
+		'label' => 'Portfolio', // this is the name of the custom post menu label, this will be the first thing that you see on the dashboard
+		'public' => true, 
 		'show_ui' => true,
 		'capability_type' => 'post',
 		'hierarchical' => false,
-		'rewrite' => array( 'slug' => 'portfolio' ),
+		'rewrite' => array( 'slug' => 'portfolio' ), // this is the category name that all the custom posts will be under
 		'query_var' => true,
 		'supports' => array(
-			'title',
-			'comments',
-			'editor',
-			'thumbnail',
-			'author',
-			'page-attributes'
+			'title', // it will support the ability to add a title 
+			'comments', // it will support the ability to add a comments
+			'editor', // it will support the ability to add a editor
+			'thumbnail', // it will support the ability to add a thumbnail
+			'author', // it will support the ability to add a author
+			'page-attributes' // it will support the ability to add a page-attributes
 		),
 		'taxonomies' => array(
 			'post_tag',
 			'category'
 		)
 	);
-	register_post_type( 'portfolio', $args );
+	register_post_type( 'portfolio', $args ); // this registers the post type title 
 }
-add_action( 'init', 'portfolio_posts' );
+add_action( 'init', 'portfolio_posts' ); // this registers the custom post type 
 
 
